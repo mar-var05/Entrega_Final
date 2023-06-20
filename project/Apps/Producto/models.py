b0c4b8148f66c05615e3ef8cd5a4e12acdd0cba3
Mariana Varon <msofiavyopasa@gmail.com>
2023-06-20 17:55:18 -0500

    Creacion de Modelo en Productos

--- project/Apps/Producto/models.py
+++ project/Apps/Producto/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Producto(models. Model):
+    Nombre_Producto = models.CharField(max_length=100, unique=True)
+    Descripcion = models.CharField(max_length=150, null=True, blank=True)
+    Precio = models.PositiveIntegerField(blank= True, null=True,default=0)    
+    Se_encuentra_disponible = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f"{self.Nombre_Producto} -- {self.Precio} -- {self.Se_encuentra_disponible}"
+    
+    class Meta:
+        verbose_name = "Producto"
+        verbose_name_plural = "Productos"
--- project/Apps/Servicio/models.py
+++ project/Apps/Servicio/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Servicio(models.Model):   
+    nombre_servicio = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=255)
+    Precio = models.PositiveIntegerField()
+    servicio_disponible = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.nombre_servicio}-- {self.Precio}--{self.servicio_disponible}"
+    
+class Meta:
+    verbose_name = "Servicio"
+    verbose_name_plural = "Servicios"
